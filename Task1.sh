15d890bd40caf22e9c6b3f5d22eeef341ff00d69
Michael De Pasquale <mickdp1@gmail.com>
2022-08-19 14:11:23 +1000

    Fix script not replacing all non-letter characters, accept input/output file paths

--- Task1.sh
+++ Task1.sh
@@ -5,21 +5,25 @@
 #
 # Description:           Task 1
 # Author:                Michael De Pasquale
-# Creation Date:         2022-08-19
-# Modification Date:     2022-08-19
 #
 ###############################################################################
 
-WORD_FILE="wlist_all/wlist_match1.txt"
+if [ "$#" != '2' ]; then
+    echo "Usage: Task1.sh {input_path} {output_path}" >&2
+    exit 1
+fi
+
+DIRTY_FILE="$1"
+CLEAN_FILE="$2"
 
 # 1. Read file
 # 2. Shuffle randomly
 # 3. Remove punctuation
 # 4. Select words of length 3 to 15 letters (inclusive)
 # 5. Sort and remove duplicates
-cat "$WORD_FILE" \
+cat "$DIRTY_FILE" \
     | shuf \
-    | sed 's/[^A-Za-z]//' \
+    | sed 's/[^A-Za-z]//g' \
     | grep -oP '^.{3,15}$' \
     | sort \
-    | uniq
+    | uniq > "$CLEAN_FILE"
